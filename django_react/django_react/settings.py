7c1260e0950a6b577c6f2b4c461040a0b9c6b3d7
Jiean Yang <jiean.yang8534@gmail.com>
2020-07-09 19:42:56 +0200

    Use .env in django

--- django_react/django_react/settings.py
+++ django_react/django_react/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'django_react.wsgi.application'
 # PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'USER': 'me',
-        'PASSWORD': 'me',
-        'HOST': 'postgres',
-        'PORT': '5432',
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        'USER': os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        'HOST': os.environ.get("SQL_HOST", "localhost"),
+        'PORT': os.environ.get("SQL_PORT", "5432"),
     }
 }
 
