57115114194e2dec1fb9c710f195e3596dcd5217
Jiean Yang <jiean.yang8534@gmail.com>
2020-07-10 11:13:42 +0200

    Staging SetUp: update Django configuration

--- django_back/django_back/settings.py
+++ django_back/django_back/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
