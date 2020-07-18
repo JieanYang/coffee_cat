754e5a9308cb9b22f9923a4a1f6d497d71aa034b
Jiean Yang <jiean.yang8534@gmail.com>
2020-07-18 22:30:47 +0200

    Debug pb of /api/ url for django_back

--- django_back/django_back/urls.py
+++ django_back/django_back/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("", image_upload, name="upload"),
     path('admin/', admin.site.urls),
 ]
-urlpatterns = [path(r'api/', include(urlpatterns))]
+# urlpatterns = [path(r'api/', include(urlpatterns))]
 
 if bool(settings.DEBUG):
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
