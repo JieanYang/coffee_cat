86f46b217626f666ee55725956622cbbca988aa3
Jiean Yang <jiean.yang8534@gmail.com>
2020-07-28 22:17:22 +0200

    Create article Reducer file

---
+++ react_front/src/store/reducers/article.js
@@ -0,0 +1,23 @@
+import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
+
+export const articlesReducer = (state=[], action) => {
+  switch(action.type) {
+    case ADD_ARTICLE:
+      return Object.assign({}, state, {
+        articles: state.articles.concat(action.payload)
+      });
+    default:
+      return state
+  }
+}
+
+export const remoteArticlesReducer = (state=[], action) => {
+  switch(action.type) {
+    case DATA_LOADED:
+      return Object.assign({}, state, {
+        remoteArticles: state.remoteArticles.concat(action.payload)
+      });
+    default:
+      return state;
+  }
+}
--- react_front/src/store/reducers/index.js
+++ react_front/src/store/reducers/index.js
@@ -6,29 +6,8 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import auth, * as fromAuth from './auth.js'
 
-import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
+import { articlesReducer, remoteArticlesReducer } from './article.js';
 
-const articlesReducer = (state=[], action) => {
-  switch(action.type) {
-    case ADD_ARTICLE:
-      return Object.assign({}, state, {
-        articles: state.articles.concat(action.payload)
-      });
-    default:
-      return state
-  }
-}
-
-const remoteArticlesReducer = (state=[], action) => {
-  switch(action.type) {
-    case DATA_LOADED:
-      return Object.assign({}, state, {
-        remoteArticles: state.remoteArticles.concat(action.payload)
-      });
-    default:
-      return state;
-  }
-}
 
 export default combineReducers({
   auth: auth,
