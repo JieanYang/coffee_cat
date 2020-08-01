60275764f8413fb0bebbc56cc1da61255288d2b9
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-01 15:08:15 +0200

    Debug echo reducer

--- react_front/src/store/reducers/index.js
+++ react_front/src/store/reducers/index.js
@@ -1,10 +1,11 @@
 // src/store/reducers/index.js
 
-export * from './echo';
+// export * from './echo'
 
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import auth, * as fromAuth from './auth.js'
+import auth, * as fromAuth from './auth'
+import echo, * as fromEcho from './echo'
 
 
 import { connectRouter } from 'connected-react-router'
@@ -13,9 +14,10 @@ import { articlesReducer, remoteArticlesReducer } from './article.js';
 
 const createRootReducer = (history) => combineReducers({
   auth: auth,
+  echo: echo,
   router: connectRouter(history), 
   articles: articlesReducer, 
-  remoteArticles: remoteArticlesReducer
+  remoteArticles: remoteArticlesReducer,
 })
 
 export default createRootReducer
@@ -26,6 +28,7 @@ export const isAccessTokenExpired =state => fromAuth.isAccessTokenExpired(state.
 export const refreshToken = state => fromAuth.refreshToken(state.auth)
 export const isRefreshTokenExpired = state => fromAuth.isRefreshTokenExpired(state.auth)
 export const authErrors = state => fromAuth.errors(state.auth)
+export const serverMessage = state => fromEcho.serverMessage(state.echo)
 
 export const withAuth = (headers={}) => {
   return (state) => ({
