099716000b53639833166b28bbacfdf5b8156b66
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-02 15:10:15 +0200

    Rename file from index to store

--- react_front/src/index.js
+++ react_front/src/index.js
@@ -5,7 +5,7 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 
 
-import configureStore from "./store";
+import configureStore from "./store/store";
 import App from "./App/App";
 
 const store = configureStore()
