112a132aeebc689ca61c005c2af2c3e6faea736c
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-02 17:39:04 +0200

    flake8 to check syntax of python

--- django_back/apps/User/views.py
+++ django_back/apps/User/views.py
@@ -5,27 +5,30 @@ from django.http import HttpResponse
 
 
 def login(request):
-	username = request.POST['username']
-	password = request.POST['password']
-	print('yang')
-	print(username)
-	print(password)
-	print('end yang')
-	user = authenticate(request, username=username, password=password)
-	if user is not None:
-		dj_login(request, user)
-		return HttpResponse("login")
-	else:
-		return HttpResponse("Response 403")
+    username = request.POST['username']
+    password = request.POST['password']
+    print('yang')
+    print(username)
+    print(password)
+    print('end yang')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        dj_login(request, user)
+        return HttpResponse("login")
+    else:
+        return HttpResponse("Response 403")
+
 
 @login_required
 def logout(request):
-	dj_logout(request)
-	return HttpResponse("logout") 
+    dj_logout(request)
+    return HttpResponse("logout")
+
 
 @login_required(login_url='/User/noauth')
 def test(request):
-	return HttpResponse("Hello, world !")
+    return HttpResponse("Hello, world !")
+
 
 def noauth(request):
-	return HttpResponse("HTTP 403 code")
+    return HttpResponse("HTTP 403 code")
