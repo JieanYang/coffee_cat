24c4f70aade5bb915d1a51db1a993256866afc52
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-11 21:42:07 +0200

    Show notes in database

--- react_front/src/App/html/1_page/Note.js
+++ react_front/src/App/html/1_page/Note.js
@@ -1,10 +1,30 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux"
 
 const Note = (props) => {
+    const [notes, setNotes] = useState([])
+
+
+    
+    useEffect(() => {
+        fetch('http://localhost:8000/Note/')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setNotes(data)
+        });
+    }, [notes.length])
+
     return (
-        <h1>Note page</h1>
+        <>
+            <h1>Note page</h1>
+            <ul>
+                {notes.map((el) => (
+                    <li key={el.pk}>{el.fields.title}</li>
+                ))}
+            </ul>
+        </>
     )
 }
 
