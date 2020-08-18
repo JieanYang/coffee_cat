f1f48df080799ab5b4d787e9d088486fdab57ba9
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-18 16:00:58 +0200

    Add background color of App

--- react_front/src/App/App.js
+++ react_front/src/App/App.js
@@ -1,20 +1,21 @@
 // src/App.js
 
-import React from "react";
+import React from "react"
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
-} from "react-router-dom";
+} from "react-router-dom"
 
+import './style/4_base/App.scss'
 
-import PrivateRoute from "./html/4_base/PrivateRoute";
-import Header from "./html/2_component/Header";
-import Article from "./html/1_page/Article";
-import Login from "./html/1_page/Login";
-import Home from "./html/1_page/Home";
-import Note from "./html/1_page/Note";
+import PrivateRoute from "./html/4_base/PrivateRoute"
+import Header from "./html/2_component/Header"
+import Article from "./html/1_page/Article"
+import Login from "./html/1_page/Login"
+import Home from "./html/1_page/Home"
+import Note from "./html/1_page/Note"
 
 const App = () => (
   <>
@@ -32,8 +33,8 @@ const App = () => (
       </div>
     </Router>
   </>
-);
+)
 
 
 
-export default App;
+export default App
