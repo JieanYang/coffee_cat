01e16a56a927ed56a49feefda7b381b04f54d1f2
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-19 23:18:35 +0200

    Add send_email views

--- django_back/apps/Email/views.py
+++ django_back/apps/Email/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import Send_Email
 
-# Create your views here.
+
+def send_email():
+    gmail_user = 'rdv.reminder666@gmail.com'
+    gmail_password = 'Pascalc4!'
+    recipient = ['dev.sb.yja@gmail.com']
+    subject = 'RDV Nanterre Prefecture Disponible'
+    url = 'http://www.hauts-de-seine.gouv.fr/booking/create/14086/0'
+    body = 'Aller sur le site ' + url 
+
+    try:
+        Send_Email.send_email(gmail_user,gmail_password, recipient,subject,body)
+    except:
+        print('An exception occurred')
+
+if __name__ == "__main__":
+    send_email()
