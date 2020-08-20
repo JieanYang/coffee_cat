d57c1ddbaa6f425a338f0bba5e21b2d065ee7d88
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-20 20:39:29 +0200

    Update EmailView

--- django_back/apps/Email/views.py
+++ django_back/apps/Email/views.py
@@ -1,20 +1,28 @@
 from django.http import HttpResponse
 from . import Send_Email
 
+from rest_framework import views as rest_views
+from rest_framework.response import Response
 
-def send_email(request):
+class EmailView(rest_views.APIView):
+    def get(request, *args, **kwargs):
+        # send_email(request)
+        return Response('nothing', status=200)
+    def post(self, request, *args, **kwargs):
+        print("POST call")
+        print(request.data)
+        send_email(request, **request.data)
+        return Response('successfully sent the mail', status=200)
+
+def send_email(request, **kwargs):
     gmail_user = 'rdv.reminder666@gmail.com'
     gmail_password = 'Pascalc4!'
-    recipient = ['dev.sb.yja@gmail.com']
-    subject = 'RDV Nanterre Prefecture Disponible'
-    url = 'http://www.hauts-de-seine.gouv.fr/booking/create/14086/0'
-    body = 'Aller sur le site ' + url 
+    recipient = [kwargs.get('recipient')]
+    subject = kwargs.get('subject')
+    body = kwargs.get('body')
+    body += ' -- supported by Jiean Yang'
 
     try:
-        Send_Email.send_email(gmail_user,gmail_password, recipient,subject,body)
-        return HttpResponse('successfully sent the mail', status=200)
+        Send_Email.send_email(gmail_user, gmail_password, recipient, subject, body)
     except:
         print('An exception occurred')
-
-if __name__ == "__main__":
-    send_email()
