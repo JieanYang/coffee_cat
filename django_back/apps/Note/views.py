f294756a957ee2e57f14362e5bc1e308a64d4278
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-26 01:05:02 +0200

    Use generics views

--- django_back/apps/Note/views.py
+++ django_back/apps/Note/views.py
@@ -1,30 +1,14 @@
 
 from .models import Note
 from .serializers import NoteSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class NoteList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class NoteList(generics.ListCreateAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class NoteDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
