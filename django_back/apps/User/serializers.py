d3c054c130ffcaea9e6055ed9913493cf8edb7d2
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-25 17:55:09 +0200

    Debug pb about user-detail and group-detail

--- django_back/apps/User/serializers.py
+++ django_back/apps/User/serializers.py
@@ -3,17 +3,20 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='User:user-detail')
-
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('id', 'url', 'username', 'email', 'groups')
+        extra_kwargs = {
+            'url': {'view_name': 'User:user-detail'}, 
+            'groups': {'view_name': 'User:group-detail'}, 
+        }
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ('url', 'name')
+        fields = ('id', 'url', 'name')
+        extra_kwargs = {'url': {'view_name': 'User:group-detail'}}  
 
 
 class MessageSerializer(serializers.Serializer):
