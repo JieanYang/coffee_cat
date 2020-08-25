80f173e3ef2e02e8ececb12525a4cdafe8817343
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-26 00:51:10 +0200

    Use class for views with pb for authorization

--- django_back/apps/Note/urls.py
+++ django_back/apps/Note/urls.py
@@ -3,6 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.note_list, name='allNotes'),
-    path('<int:pk>/', views.note_detail, name='oneNote'),
+    path('', views.NoteList.as_view(), name='allNotes'),
+    path('<int:pk>/', views.NoteDetail.as_view(), name='oneNote'),
 ]
--- django_back/apps/Note/views.py
+++ django_back/apps/Note/views.py
@@ -1,62 +1,57 @@
 
 from django.http import HttpResponse
 
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-
 from .models import Note
 from .serializers import NoteSerializer
 
+from rest_framework.renderers import JSONRenderer
 from rest_framework import status
-from rest_framework.decorators import api_view
-
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from django.utils.decorators import method_decorator
 
 
 class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
     def __init__(self, data, **kwargs):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-@csrf_exempt
-# @api_view(['GET', 'POST'])
-def note_list(request, format=None):
-    if request.method == 'GET':
+class NoteList(APIView):
+    def get(self, request, format=None):
         notes = Note.objects.all()
         serializer = NoteSerializer(notes, many=True)
         return JSONResponse(serializer.data)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = NoteSerializer(data=data)
+
+    def post(self, request, format=None):
+        serializer = NoteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JSONResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JSONResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-# @api_view(['GET', 'PUT', 'DELETE'])
-def note_detail(request, pk, format=None):
-    try:
-        note = Note.objects.get(pk=pk)
-    except Note.DoesNotExist:
-        return JSONResponse(status=status.HTTP_404_NOT_FOUND)
+class NoteDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Note.objects.get(pk=pk)
+        except Note.DoesNotExist:
+            return JSONResponse(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        note = self.get_object(pk)
         serializer = NoteSerializer(note)
         return JSONResponse(serializer.data)
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = NoteSerializer(note, data=data)
+
+    def put(self, request, pk, format=None):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JSONResponse(serializer.data)
         return JSONResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        note = self.get_object(pk)
         note.delete()
         return JSONResponse(status=status.HTTP_204_NO_CONTENT)
