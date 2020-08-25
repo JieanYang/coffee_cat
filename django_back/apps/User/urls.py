b8fe7e1459a1cfc91c75875e8e26a297f1329d57
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-25 16:31:15 +0200

    Create serializers and views for User and Group, remove useless codes

--- django_back/apps/User/urls.py
+++ django_back/apps/User/urls.py
@@ -1,5 +1,3 @@
-from django.urls import path, include
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -7,31 +5,11 @@ from rest_framework_simplejwt.views import (
 
 from . import views
 
-# temporary
-from rest_framework import views as rest_views, serializers, status
-from rest_framework.response import Response
-
-
-class MessageSerializer(serializers.Serializer):
-    message = serializers.CharField()
-
-
-class EchoView(rest_views.APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = MessageSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-# temporary
 
 
 app_name = 'User'
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='results'),
-    path('test/', views.test, name='test'),
-    path('noauth/', views.noauth, name='noauth'),
-
     path(r'auth/token/obtain/', TokenObtainPairView.as_view()),
     path(r'auth/token/refresh/', TokenRefreshView.as_view()),
-    path(r'echo/', EchoView.as_view())
+    path(r'echo/', views.EchoView.as_view())
 ]
