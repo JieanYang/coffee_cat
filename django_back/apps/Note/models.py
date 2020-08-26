657cf55616b09c5bca7ee2377ce828f75dd483cd
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-26 23:35:00 +0200

    Add ForeignKey in Note and PrimaryKey in User

--- django_back/apps/Note/models.py
+++ django_back/apps/Note/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=2000)
     created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User', related_name='notes', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('created',)
--- django_back/apps/User/serializers.py
+++ django_back/apps/User/serializers.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User, Group
+from apps.Note.models import Note
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    notes = serializers.PrimaryKeyRelatedField(many=True, queryset=Note.objects.all())
+
     class Meta:
         model = User
-        fields = ('id', 'url', 'username', 'email', 'groups')
+        fields = ('id', 'url', 'username', 'email', 'groups', 'notes')
         extra_kwargs = {
             'url': {'view_name': 'User:user-detail'},
             'groups': {'view_name': 'User:group-detail'},
