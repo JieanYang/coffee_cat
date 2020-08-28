bda6f38626a8c0b886ff93030202addcda52d6e5
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-28 19:48:08 +0200

    Recompose auth acitons and reducer

--- react_front/src/store/actions/auth.js
+++ react_front/src/store/actions/auth.js
@@ -1,24 +1,15 @@
-import { RSAA } from 'redux-api-middleware'
 
-export const LOGIN_REQUEST = '@@auth/LOGIN_REQUEST'
-export const LOGIN_SUCCESS = '@@auth/LOGIN_SUCCESS'
-export const LOGIN_FAILURE = '@@auth/LOGIN_FAILURE'
-export const TOKEN_REQUEST = '@@auth/TOKEN_REQUEST'
-export const TOKEN_RECEIVED = '@@auth/TOKEN_RECEIVED'
-export const TOKEN_FAILURE = '@@auth/TOKEN_FAILURE'
-export const LOGOUT = "@@auth/LOGOUT"
+export const LOGIN = 'Auth/login'
+export const LOGOUT = "Auth/logout"
 
-export const login = (username, password) => ({
-  [RSAA]: {
-    endpoint: APP_BACK_ENDPOINT + '/User/auth/token/obtain/',
+export const login = (username='admin', password='admin') => {
+  fetch(`${APP_BACK_ENDPOINT}/api-token-auth/`, {
     method: 'POST',
-    body: JSON.stringify({username, password}),
-    headers: { 'Content-Type': 'application/json' },
-    types: [
-      LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE
-    ]
-  }
-})
+    body: JSON.stringify({username, password})
+  }).then(response => {
+    return {type: LOGIN, payload: response}
+  })
+}
 
 export const logout = () => {
   return { type: LOGOUT }
--- react_front/src/store/reducers/auth.js
+++ react_front/src/store/reducers/auth.js
@@ -1,74 +1,22 @@
 import jwtDecode from 'jwt-decode'
-import { LOGIN_SUCCESS, TOKEN_RECEIVED, LOGIN_FAILURE, TOKEN_FAILURE, LOGOUT } from '../actions'
+import { LOGIN, LOGOUT } from '../actions'
 
 const initialState = {
-  access: undefined,
-  refresh: undefined,
-  errors: {},
+  token: undefined,
+  errors: {}
 }
 
 export const authReducer = (state=initialState, action) => {
   switch(action.type) {
-    case LOGIN_SUCCESS:
+    case LOGIN:
       return {
-        access: {
-          token: action.payload.access,
-          ...jwtDecode(action.payload.access)
-        },
-        refresh: {
-          token: action.payload.refresh,
-          ...jwtDecode(action.payload.refresh)
-        },
-        errors: {}
-      }
-    case TOKEN_RECEIVED:
-      return {
-        ...state,
-        access: {
-          token: action.payload.access,
-          ...jwtDecode(action.payload.access)
-        }
-      }
-    case LOGIN_FAILURE:
-    case TOKEN_FAILURE:
-      return {
-         access: undefined,
-         refresh: undefined,
-         errors: 
-             action.payload.response || 
-                {'non_field_errors': action.payload.statusText},
+        token: action.payload
       }
     case LOGOUT:
       return initialState
     default:
       return state
     }
-}
-
-const accessToken = (state) => {
-    if (state.auth.access) {
-        return  state.auth.access.token
-    }
-}
-
-const isRefreshTokenExpired = (auth) => {
-  if (auth.refresh && auth.refresh.exp) {
-    return 1000 * auth.refresh.exp - (new Date()).getTime() < 5000
-  }
-  return true
-}
-    
-export const refreshToken = (state) => {
-    if (state.auth.refresh) {
-        return  state.auth.refresh.token
-    }
-}
-    
-export const isAccessTokenExpired = (state) => {
-  if (state.auth.access && state.auth.access.exp) {
-    return 1000 * state.auth.access.exp - (new Date()).getTime() < 5000
-  }
-  return true
 }
 
 export const authErrors = (state) => {
@@ -79,6 +27,6 @@ export const authErrors = (state) => {
 export const withAuth = (headers={}) => {
   return (state) => ({
     ...headers,
-    'Authorization': `Bearer ${accessToken(state)}`
+    'Authorization': ``
   })
 }
