036618573a79fd966a86b625c83b124b69580f43
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-30 17:22:06 +0200

    Debug problem to send email

--- react_front/src/store/actions/email.js
+++ react_front/src/store/actions/email.js
@@ -1,20 +1,18 @@
-export const SEND_EMAIL = 'SEND_EMAIL'
 import { withAuth } from '../reducers'
-import { RSAA } from 'redux-api-middleware';
+
+export const SEND_EMAIL = 'SEND_EMAIL'
 
 
-export const SEND_EMAIL_REQUEST = '@@Email/SEND_EMAIL_REQUEST'
-export const SEND_EMAIL_SUCCESS = '@@Email/SEND_EMAIL_SUCCESS'
-export const SEND_EMAIL_FAILURE = '@@Email/SEND_EMAIL_FAILURE'
-
-export const sendEmail = (data) => ({
-    [RSAA]: {
-        endpoint: APP_BACK_ENDPOINT + '/Email/send_email',
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: withAuth({ 'Content-Type': 'application/json' }),
-        types: [
-            SEND_EMAIL_REQUEST, SEND_EMAIL_SUCCESS, SEND_EMAIL_FAILURE
-        ]
-    }
-})
+export const sendEmail = ({recipient, subject, body}) => (dispatch, getState) => {
+    fetch(APP_BACK_ENDPOINT + '/Email/send_email', {
+        method: 'POST', 
+        headers: withAuth(getState())(),
+        body: JSON.stringify({recipient, subject, body})
+    })
+    .then(resposne => resposne.json().then(data => ({status: resposne.status, data})))
+    .then(({status, data}) => {
+        console.log('status', status)
+        console.log('data', data)
+        dispatch({type: "Suceess send email"})
+    })
+}
