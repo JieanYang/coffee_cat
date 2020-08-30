22d905be33f4a7946370eb53be84db4b9721c6f2
Jiean Yang <jiean.yang8534@gmail.com>
2020-08-30 10:42:13 +0200

    Optimize code in Note/views

--- django_back/apps/Note/views.py
+++ django_back/apps/Note/views.py
@@ -9,14 +9,13 @@ from .permissions import IsOwnerOrReadOnly
 class NoteList(generics.ListCreateAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly,)
 
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
