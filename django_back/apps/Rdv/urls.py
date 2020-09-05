ccb05239b4acb8a0be36a425c776bcdde94f7823
Jiean Yang <jiean.yang8534@gmail.com>
2020-09-05 19:08:53 +0200

    Create RevView

--- django_back/apps/Rdv/urls.py
+++ django_back/apps/Rdv/urls.py
@@ -1,10 +1,27 @@
+from django.urls import path, include
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import routers
 from .views import OrderViewSet, ProductViewSet, PrefectureViewSet
 
+class RdvView(APIView):
+  """
+    Api for order, product and prefecture
+    """
+  def get(self, request, *args, **kwargs):
+    apidocs = {
+      'orders': request.build_absolute_uri('orders/'),
+      'products': request.build_absolute_uri('products/'),
+      'prefectures': request.build_absolute_uri('prefectures/')
+    }
+    return Response(apidocs)
 
 app_name = 'Rdv'
 router = routers.DefaultRouter()
 router.register('orders', OrderViewSet)
 router.register('products', ProductViewSet)
 router.register('prefectures', PrefectureViewSet)
-urlpatterns = router.urls
+urlpatterns = [
+  path('', RdvView.as_view()),
+  path('', include(router.urls)),
+]
